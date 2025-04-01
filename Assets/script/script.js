dbfd5ceee62ae73ccc0508d7d89db47f977b0bdd
DiazMan27 <diazgithub27@gmail.com>
2025-04-01 14:53:12 -0300

    subi los archivos

---
+++ Assets/script/script.js
@@ -0,0 +1,25 @@
+// Cambio de nombre dinámico
+document.getElementById("nombre").innerText = "Leandro";
+
+const nombreElement = document.getElementById("nombre");
+const texto = "Leandro"; // Nombre que se va a escribir y borrar
+let index = 0;
+let isDeleting = false;
+
+function escribir() {
+    if (!isDeleting && index < texto.length) {
+        nombreElement.innerText += texto.charAt(index);
+        index++;
+        setTimeout(escribir, 150); // Velocidad de escritura
+    } else if (isDeleting && index > 0) {
+        nombreElement.innerText = texto.substring(0, index - 1);
+        index--;
+        setTimeout(escribir, 100); // Velocidad de borrado
+    } else {
+        isDeleting = !isDeleting; // Cambiar de modo: escribir o borrar
+        setTimeout(escribir, 1000); // Esperar un poco antes de empezar a borrar
+    }
+}
+
+escribir(); // Iniciar la animación
+
